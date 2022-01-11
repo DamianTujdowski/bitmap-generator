6eb7b793162899c20e0ff94a686e98dc9c017e14
DamianTujdowski <d.tujdowski@gmail.com>
2022-01-11 23:54:53 +0100

    Added map to store interval color values for blue

--- src/main/java/LinearGradientGenerator.java
+++ src/main/java/LinearGradientGenerator.java
@@ -2,18 +2,29 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class LinearGradientGenerator implements ImageGenerator {
     private final BufferedImage image;
     private final int width;
     private final int height;
+    private Map<Integer, Integer> blueHorizontalGradientValues;
+    //TODO move RGB to another class?
+    private int red;
+    private int green;
+    private int blue;
 
     public LinearGradientGenerator(BufferedImage image) {
         this.image = image;
         width = image.getWidth();
         height = image.getHeight();
+        blueHorizontalGradientValues = new LinkedHashMap<>();
+        red = 255;
+        green = 124;
+        blue = 20;
     }
-
 
     @Override
     public void generateImage() {
@@ -40,17 +51,33 @@ public class LinearGradientGenerator implements ImageGenerator {
         }
     }
 
+//    private void generateHorizontalLinearGradient() {
+//        int interval = computeHorizontalInterval(blue);
+//        int tempInterval = interval;
+//        for (int y = 0; y < width; y++) {
+//            if (tempInterval > 0) {
+//                tempInterval--;
+//            } else if (tempInterval == 0) {
+//                blue++;
+//                tempInterval = interval;
+//            }
+//
+//            int pixel = (red << 16) | (green << 8) | blue;
+//
+//            for (int x = 0; x < height; x++) {
+//                image.setRGB(y, x, pixel);
+//            }
+//        }
+//    }
+
     private void generateHorizontalLinearGradient() {
-        int red = 255, blue = 20, green = 124;
-        int interval = width / (255 - blue);
-        int tempInterval = interval;
+        fillBlueHorizontalGradientValues();
+
+        int blueInterval = computeHorizontalInterval(blue);
+
         for (int y = 0; y < width; y++) {
-            if (tempInterval > 0) {
-                tempInterval--;
-            } else if (tempInterval == 0) {
-                blue++;
-                tempInterval = interval;
-            }
+            int key = y - y % blueInterval;
+            blue = blueHorizontalGradientValues.get(key);
 
             int pixel = (red << 16) | (green << 8) | blue;
 
@@ -60,10 +87,23 @@ public class LinearGradientGenerator implements ImageGenerator {
         }
     }
 
+    private void fillBlueHorizontalGradientValues() {
+        int interval = computeHorizontalInterval(blue);
+        final int[] tempBlue = {blue};
+
+        IntStream.iterate(0, n -> n + interval)
+                .limit(255 - blue)
+                .forEach(num -> blueHorizontalGradientValues.put(num, tempBlue[0]++));
+    }
+
+    private int computeHorizontalInterval(int color) {
+        return width / (255 - color);
+    }
+
     @Override
     public void saveImage() {
         try {
-            File file = new File("src\\pics\\linearGradient.jpeg");
+            File file = new File("src\\main\\java\\pics\\linearGradient.jpeg");
             ImageIO.write(image, "jpeg", file);
         } catch (IOException e) {
             e.printStackTrace();
